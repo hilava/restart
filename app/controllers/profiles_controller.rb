012c646d5fafb82807037d425a5c59c63a4d09b7
Hila Vasiler <hila.marmel@gmail.com>
2016-05-27 12:36:30 -0700

    created profile controller methods

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,13 +1,65 @@
 class ProfilesController < ApplicationController
   def index
+    @profiles = Profile.all
   end
 
   def show
+    @profile = Profile.find(params[:id])
   end
 
   def new
+    if current_user
+      @profile = Profile.new
+      @user = current_user
+      render :new
+    else
+      flash[:alert] = "Please login/sign up in order to create a profile"
+      redirect_to root_path
+    end
   end
 
+def create
+  profile = Profile.new(profile_params)
+  profile.user = current_user
+  if profile.save
+    flash[:notice] = "Profile saved successfully"
+    redirect_to profile_path
+  else
+    flash[:error] = profile.errors.full_messages.join(", ")
+    redirect_to new_profile_path
+  end
+end
+
   def edit
+    @profile = Profile.find(params[:id])
+    @user = current_user
+    render :edit
+  end
+
+  def update
+    @profile = Profile.find(params[:id])
+    if profile.update_attributes(profile_params)
+      flash[:notice] = "Profile updated successfully"
+      redirect_to root_path
+    else
+      flash[:error] = @profile.erorrs.full_messages.join(", ")
+      redirect_to edit_profile_path
+    end
+  end
+
+  def destroy
+    @profile = Profile.find(params[:id])
+    if @profile.destroy
+      flash[:notice]  = "Profile deleted successfully"
+      redirect_to root_path
+    else
+      flash[:error] = @profile.errors.full_messages.join(", ")
+    end
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:country, :state, :city, :user_id, :image, :linkedin, :resume)
   end
 end
